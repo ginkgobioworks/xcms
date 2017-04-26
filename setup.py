cd991bbfd89afd316095f2d012b1d994683075ee
Gitlab Build 13355 <misha@ginkgobioworks.com>
2017-04-26 13:51:39 -0400

    Bump version: 1.39.2+ginkgo2 → 1.39.2+ginkgo3

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ class DevelopCommandWithXcms(develop):
 
 setup(
   name='xcms',
-  version='1.39.2+ginkgo2',
+  version='1.39.2+ginkgo3',
 
   description="Ginkgo Bioworks' forked extensions and fixes to XCMS",
   long_description=open('README.md', 'r').read(),
